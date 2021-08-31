e91fe6d07347d2453e962199dd352969982f5b20
dada4747 <adsurerahul96@gmail.com>
2021-08-31 08:52:45 +0530

    ADDED: cocoapod lib template for fetch image from url and set uiimageview

---
+++ Example/Pods/Target Support Files/FetchAndSetImage/FetchAndSetImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FetchAndSetImage : NSObject
+@end
+@implementation PodsDummy_FetchAndSetImage
+@end
---
+++ Example/Pods/Target Support Files/Pods-FetchAndSetImage_Example/Pods-FetchAndSetImage_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FetchAndSetImage_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FetchAndSetImage_Example
+@end
